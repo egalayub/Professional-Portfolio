c61e8e4cfe6bc2eaa5b4a48932c49829e89e82b1
Ayub <egalayub2@gmail.com>
2023-04-23 03:06:12 -0400

    Updated Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,6 +12,27 @@ const Projects = () => {
 </div>
 {/* container */}
 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+     {/* Grid Item */}
+     <div style={{backgroundImage: `url(${meals})`}}
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        {/* Hover effect */}
+        <div className='opacity-0 group-hover:opacity-100'>
+            <span className='text-2xl font-bold text-white tracking-wider'>
+        Meals By Dads
+            </span>
+            <div className='pt-8 text-center'>
+                <span>
+                    
+                </span>
+                <div>
+                    <a href="https://github.com/egalayub/MealsByDad"target="_blank" rel="noreferrer">
+                        <button className='text-center rounded-lg px-4 py-3 my-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
     {/* Grid Item */}
     <div style={{backgroundImage: `url(${myPet})`}}
     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
@@ -55,27 +76,7 @@ SpaceInvaders
             </div>
         </div>
     </div>
-      {/* Grid Item */}
-      <div style={{backgroundImage: `url(${meals})`}}
-    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-        {/* Hover effect */}
-        <div className='opacity-0 group-hover:opacity-100'>
-            <span className='text-2xl font-bold text-white tracking-wider'>
-        Meals By Dads
-            </span>
-            <div className='pt-8 text-center'>
-                <span>
-                    
-                </span>
-                <div>
-                    <a href="/">
-                        <button className='text-center rounded-lg px-4 py-3 my-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                    </a>
-                </div>
-
-            </div>
-        </div>
-    </div>
+     
 
 
     
