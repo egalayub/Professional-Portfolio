afdade30601935271c489ba5dc32721671b2273c
Ayub <egalayub2@gmail.com>
2023-05-08 18:20:05 -0400

    Added CryptoPedia Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import meals from '../assets/mbydad.jpg';
 import myPet from '../assets/mypet.jpg';
 import invader from '../assets/invaders.jpg';
+import CryptoPedia from '../assets/CryptoPedia.jpg';
 const Projects = () => {
   return (
     <div name = 'work' className='w-ful md:h-screen text-gray-300 bg-green-950'>
@@ -12,6 +13,27 @@ const Projects = () => {
 </div>
 {/* container */}
 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+     {/* Grid Item */}
+     <div style={{backgroundImage: `url(${CryptoPedia})`}}
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        {/* Hover effect */}
+        <div className='opacity-0 group-hover:opacity-100'>
+            <span className='text-2xl font-bold text-white tracking-wider'>
+        CryptoPedia
+            </span>
+            <div className='pt-8 text-center'>
+                <span>
+                    
+                </span>
+                <div>
+                    <a href="https://github.com/egalayub/Investing-App"target="_blank" rel="noreferrer">
+                        <button className='text-center rounded-lg px-4 py-3 my-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
      {/* Grid Item */}
      <div style={{backgroundImage: `url(${meals})`}}
     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
